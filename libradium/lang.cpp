a67084702e9d5d932be7761a1226e4e5df0402a0
johtsch <johannestschapka@gmail.com>
2017-11-30 20:00:40 +0100

    .lang-Dateien koennen jetzt komplett ausgelesen werden

--- libradium/lang.cpp
+++ libradium/lang.cpp
@@ -31,9 +31,9 @@ bool Lang::loadFile(std::string fpath){
        return false;
     }
 
-    _file.open(fpath);
+    _file.open(fpath, std::ios::in);
 
-    if(!_file){
+    if(!_file.good()){
         _status = "Failed to open File: " + fpath;
         return false;
     }
@@ -47,8 +47,12 @@ bool Lang::start(){
 }
 
 bool Lang::init(){
+    _file.seekg(std::ios::beg);
     std::string tmp;
-    while((tmp=analyse())!= LANG_B_IMPLEMENTATION){
+    while(true){
+
+        tmp=analyse();
+
         if(tmp == LANG_B_VAR){
             //analyseVar();
             _status = "VAR-Teil erreicht";
@@ -65,6 +69,7 @@ bool Lang::init(){
             _status = "IMPLEMENTATION-Teil erreicht";
             if(!_quiet)
                 std::cout << "Lang: init(): " << _status << std::endl;
+            break;
         }    
         if(tmp == LANG_EOF){
             _status = "File does NOT contain an IMPLEMENTATION environment -> can't be executed!";
@@ -77,21 +82,29 @@ bool Lang::init(){
 }
 
 std::string Lang::analyse(){
-    std::string line="";
+    char line[1024];
     std::string word ="";
 
     bool ret = false;
 
     while(true){
         _file.clear();                      /* damit, wenn nur < 250 Byte ausgelesen werden können, trz weitergemacht wird.
-                                               die schlechten bits wie ios::fail werden durch clear zurückgesetzt. */
+        //                                       die schlechten bits wie ios::fail werden durch clear zurückgesetzt. */
         //std::getline(_file, line);
-        _file.getline(line, 250);
-        for(int i=0; i < line.size(); ++i){
-            if(line[i]!=' ' && line[i] != '\n'){
+        memset(line, '\0', 1024);
+        _file.getline(line, 1024);
+        word.clear();
+        std::cout << " > " << line << std::endl;
+        
+        for(int i=0; i < sizeof(line); i++){
+            if(line[i] != ' ' && line[i] != '\0' && line[i] != '\t' && line[i] != ';'){             /* !!WICHTIG!! es muss auf '\0' anstelle von '\n' geprüft werden, da getline nicht
+                                                                                                       den Zeilenumbruch mitliefert, sondern den String beendet, wenn es '\n' erreicht */
                 word+=line[i];
             }
             else{
+                if(word.size() > 2)
+                    std::cout << " >>> " << word << std::endl;
+
                 ret |= (word == LANG_B_IMPLEMENTATION);
                 ret |= (word == LANG_B_VAR);
                 ret |= (word == LANG_B_STEP);
@@ -103,10 +116,11 @@ std::string Lang::analyse(){
 
                 if(ret)
                     return word;
-                else
-                    word = "";
+
+                word.clear();
             }
         }
+
         if(_file.eof())
             return LANG_EOF; 
     }
