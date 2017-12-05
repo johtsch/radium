9c6d28a0fa50971e6ae4228610ee08362c6c05d3
johtsch <johannestschapka@gmail.com>
2017-12-05 20:20:45 +0100

    Neue Hilfsfunktionen.
    Neue Hilfsfunktionen getVarType() und getVarTypeStr(), welche dazu dienen zwischen den Konstanten für die Datentypenbezeichner zu konvertieren. Außerdem gibt es ein neues Konstantenpaar, das *_INVALID-Paar, das angibt, dass der Bezeichner nicht vorhanden/unbekannt ist.

--- libradium/vartypes.cpp
+++ libradium/vartypes.cpp
@@ -16,3 +16,37 @@ bool isVartype(std::string word){
 
     return false;
 }
+
+unsigned char getVarType(std::string type){
+    if(type == LG_VT_HADDR)
+        return VARTYPE_HADDR;
+    if(type == LG_VT_IPADDR)
+        return VARTYPE_IPADDR;
+    if(type == LG_VT_PORT)
+        return VARTYPE_PORT;
+    if(type == LG_VT_BYTE)
+        return VARTYPE_BYTE;
+    if(type == LG_VT_SHORT)
+        return VARTYPE_SHORT;
+    if(type == LG_VT_INT)
+        return VARTYPE_INT;
+
+    return VARTYPE_INVALID;
+}
+
+std::string getVarTypeStr(unsigned char type){
+    if(type == VARTYPE_HADDR)
+        return LG_VT_HADDR;
+    if(type == VARTYPE_IPADDR)
+        return LG_VT_IPADDR;
+    if(type == VARTYPE_PORT)
+        return LG_VT_PORT;
+    if(type == VARTYPE_BYTE)
+        return LG_VT_BYTE;
+    if(type == VARTYPE_SHORT)
+        return LG_VT_SHORT;
+    if(type == VARTYPE_INT)
+        return LG_VT_INT;
+
+    return LG_VT_INVALID;
+}
--- libradium/vartypes.hpp
+++ libradium/vartypes.hpp
@@ -4,24 +4,27 @@
 #include <iostream>
 #include <string>
 
-#define VARTYPE_HADDR   1       // Hardwareaddresse
-#define VARTYPE_IPADDR  2       // IPv4-Addresse
-#define VARTYPE_PORT    3       // TCP/UDP-Port
-#define VARTYPE_BYTE    4
-#define VARTYPE_SHORT   5
-#define VARTYPE_INT     6
+/* unterschiedliche Variablentyp-Codes */
+const unsigned char VARTYPE_INVALID = 0;       // wenn Datentyp nicht zugeordnet werden kann
+const unsigned char VARTYPE_HADDR   = 1;       // Hardwareaddresse
+const unsigned char VARTYPE_IPADDR  = 2;       // IPv4-Addresse
+const unsigned char VARTYPE_PORT    = 3;       // TCP/UDP-Port
+const unsigned char VARTYPE_BYTE    = 4;
+const unsigned char VARTYPE_SHORT   = 5;
+const unsigned char VARTYPE_INT     = 6;
 
-const std::string LG_VT_HADDR = "HADDR";
-const std::string LG_VT_IPADDR = "IP";
-const std::string LG_VT_PORT = "PORT";
-const std::string LG_VT_BYTE = "BYTE";
-const std::string LG_VT_SHORT = "SHORT";
-const std::string LG_VT_INT = "INT"; 
+const std::string LG_VT_INVALID     = "INVALID";
+const std::string LG_VT_HADDR       = "HADDR";
+const std::string LG_VT_IPADDR      = "IP";
+const std::string LG_VT_PORT        = "PORT";
+const std::string LG_VT_BYTE        = "BYTE";
+const std::string LG_VT_SHORT       = "SHORT";
+const std::string LG_VT_INT         = "INT"; 
 
 struct varinfo{
-    char _type;
-    std::string _name;
-    short _index;                   // der Index der Variable innerhalb der Vector-Struktur in der alle Variablen des gleichen Typs gespeichert werden
+    unsigned char _type;            // siehe VARTYPE_*
+    std::string _name;              // Name der Variable innerhalb der .lang-Datei
+    long unsigned int _index;                   // der Index der Variable innerhalb der Vector-Struktur in der alle Variablen des gleichen Typs gespeichert werden
 };
 
 struct port{
@@ -34,5 +37,9 @@ typedef unsigned char byte;
 //Funktionen
 
 bool isVartype(std::string word);
+
+unsigned char getVarType(std::string type);
+
+std::string getVarTypeStr(unsigned char type);
 
 #endif
