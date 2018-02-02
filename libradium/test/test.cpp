c6776e8951d9ca3b9e166490e657371dcbaae379
johtsch <johannestschapka@gmail.com>
2018-02-02 19:34:48 +0100

    -

--- libradium/test/test.cpp
+++ libradium/test/test.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char **argv){
     Lang langtest;
     langtest.setQuiet(false);
-    langtest.loadFile("../../langfiles/icmptest.lang");
+    langtest.loadFile("../../langfiles/synflood2.lang");
     langtest.start();
     
     std::cout << std::endl;
